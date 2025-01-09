4beaa5181eba62f6383dc453d05800d014021308
cidragon <darioperezpastrian@gmail.com>
2025-01-09 19:32:10 -0300

    show grid

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,25 @@
 
-
 export default function Home() {
+
+  const create_grid = () => {
+    const grid_size : number = 20
+    const divs = []
+    for(let y = 0; y < grid_size; y++){
+      for(let x = 0; x < grid_size; x++){
+        const cell_key : string = x + "," + y
+        const cell = <div key={cell_key} className={"grid-cell "} ></div>
+        //cell_divs.set(cell_key, cell)
+        divs.push(cell);
+      }
+    }
+    return divs
+  }
+
   return (
-    <div>
-      Empty page
+    <div className="content">
+      <div className="cells">
+        {create_grid()}
+      </div>
     </div>
   );
 }
